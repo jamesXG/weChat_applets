451720ab5ab38d50576ece21d2f1ba27fcf813f7
james <1186691601@qq.com>
2018-05-23 11:13:01 +0800

    private data
    private data

--- utils/config.js
+++ utils/config.js
@@ -3,14 +3,13 @@ class Config {
 
   }
 }
-// 网络：https://myjjxy.top/map_hnlg-api/public/index.php/api/v1
 // 本地：http://map_hnlg.cn/index.php/api/v1
 // API接口根url
-Config.baseUrl = 'https://myjjxy.top/map_hnlg-api/public/index.php/api/v1';
+Config.baseUrl = 'http://map_hnlg.cn/index.php/api/v1';
 // 缓存有效期(毫秒)
 Config.expired = 7200000;
 
-Config.key = 'J5RBZ-ZINWQ-MWT5D-GHK6G-O6JPJ-D6B7F';
+Config.key = '*****-*****-*****-*****-*****-*****'; //在腾讯地图开放平台可以申请
 
 // 地图导航Api
 Config.mapApi = 'https://apis.map.qq.com/ws/direction/v1/';
